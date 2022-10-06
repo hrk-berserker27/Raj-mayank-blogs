7de37f7a2dffe2baf8d20ac1fe444cdc6a44442c
Hritick <hritickbhushan@gmail.com>
2022-10-06 23:56:07 +0530

    Added the slider counter

--- components/BlogTable.js
+++ components/BlogTable.js
@@ -12,15 +12,25 @@ function BlogTable() {
     if (typeof window !== "undefined") {
       let interval;
       const slider = document.getElementById("slider");
+      const labelList = document.getElementById("list");
+      const array = labelList.children;
       const childNumber = slider.childElementCount;
       if (inview && counter < childNumber) {
         interval = setTimeout(() => {
           slider.setAttribute("style", `margin-left: calc(${-counter}*100%)`);
+          array[counter].setAttribute(
+            "style",
+            `background-color: rgb(217, 214, 214)`
+          );
+          if (counter > 0) {
+            array[counter - 1].removeAttribute("style");
+          }
           setCounter((prevState) => prevState + 1);
         }, 5000);
       } else {
         if (counter === childNumber) {
           setCounter((prevState) => prevState - prevState);
+          array[counter - 1].removeAttribute("style");
         }
       }
       return () => {
@@ -38,11 +48,8 @@ function BlogTable() {
           <div className={styles.slides}>3</div>
           <div className={styles.slides}>4</div>
         </div>
-        <div className={styles.slidesCounter}>
-          <label
-            className={`${styles.dash} ${styles.active}`}
-            htmlFor="1"
-          ></label>
+        <div className={styles.slidesCounter} id="list">
+          <label className={styles.dash} htmlFor="1"></label>
           <label className={styles.dash} htmlFor="2"></label>
           <label className={styles.dash} htmlFor="3"></label>
           <label className={styles.dash} htmlFor="4"></label>
