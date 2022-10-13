28b32eee274d160b8ecd2a0973aa0b44d61e310f
Hritick <hritickbhushan@gmail.com>
2022-10-13 22:54:11 +0530

    Added some changes

--- pages/api/blog.js
+++ pages/api/blog.js
@@ -1,15 +1,15 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import clientPromise from "../../lib/mongodb";
+// // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+// import clientPromise from "../../lib/mongodb";
 
-export default async (req, res) => {
-  try {
-    const client = await clientPromise;
-    const db = client.db("test");
+// export default async (req, res) => {
+//   try {
+//     const client = await clientPromise;
+//     const db = client.db("test");
 
-    const posts = await db.collection("posts").find({}).toArray();
+//     const posts = await db.collection("posts").find({}).toArray();
 
-    res.json(posts);
-  } catch (e) {
-    console.error(e);
-  }
-};
+//     res.json(posts);
+//   } catch (e) {
+//     console.error(e);
+//   }
+// };
