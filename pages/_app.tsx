df4922e5995fc836ff9cc56db1b0e08e5b15c952
Hritick <hritickbhushan@gmail.com>
2022-10-15 21:53:34 +0530

    Added some files

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,6 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
 
-// function MyApp({ Component, pageProps }) {
-//   return <Component {...pageProps} />
-// }
-import type { AppProps } from 'next/app'
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
