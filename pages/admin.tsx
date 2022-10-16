2cc990ac8c9a2ced266a149e22708225bd373e46
Hritick <hritickbhushan@gmail.com>
2022-10-16 23:35:43 +0530

    Added form

--- pages/admin.tsx
+++ pages/admin.tsx
@@ -1,12 +1,19 @@
+import { FormEvent } from "react";
 import styles from "../styles/LoginPage.module.css";
 function login() {
-  return <div className={styles.formContainer}>
+
+  const handleSubmit = (e: FormEvent<HTMLInputElement>)=> {
+    e.preventDefault();
+    console.log("hello");
+  }
+  return(
+  <div className={styles.formContainer}>
     <form className={styles.form}>
       <h1>Admin Login</h1>
     <input type="text" /><input type="password" autoComplete="on"/>
-    <input type="submit"></input>
+    <input type="submit" onSubmit={handleSubmit}></input>
     </form>
-    </div>;
+    </div>);
 }
 
 export default login;
