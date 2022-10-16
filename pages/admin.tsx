45204f05125b39e7d8b0922f46cf8ed9634ca269
Hritick <hritickbhushan@gmail.com>
2022-10-16 23:44:02 +0530

    Fixed some issues with form and changed the design of the form

--- pages/admin.tsx
+++ pages/admin.tsx
@@ -2,16 +2,16 @@ import { FormEvent } from "react";
 import styles from "../styles/LoginPage.module.css";
 function login() {
 
-  const handleSubmit = (e: FormEvent<HTMLInputElement>)=> {
+  const handleSubmit = (e: FormEvent<HTMLFormElement>)=> {
     e.preventDefault();
     console.log("hello");
   }
   return(
   <div className={styles.formContainer}>
-    <form className={styles.form}>
+    <form className={styles.form} onSubmit={handleSubmit}>
       <h1>Admin Login</h1>
     <input type="text" /><input type="password" autoComplete="on"/>
-    <input type="submit" onSubmit={handleSubmit}></input>
+    <input type="submit"></input>
     </form>
     </div>);
 }
